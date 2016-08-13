f6d5f8a4ef9c861d94390e4edb078c696a592644
Michael Jackson <mjijackson@gmail.com>
2016-08-13 16:28:39 -0700

    Add build/serve scripts

---
+++ scripts/build.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+const execSync = require('child_process').execSync
+
+const exec = (command) =>
+  execSync(command, { stdio: 'inherit' })
+
+const name = process.argv[2]
+const baseDir = path.resolve(__dirname, '..', name)
+
+fs.stat(baseDir, (error, stats) => {
+  if (error) {
+    console.error(error.message)
+    process.exit(1)
+  }
+
+  exec(`webpack --config ${baseDir}/webpack.config.js`)
+})
